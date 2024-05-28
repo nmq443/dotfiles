106bf96af5d742639f89f2a91c56a5cbb46b5040
nmq443 <210202564@vnu.edu.vn>
2024-05-28 21:28:56 +0700

    edit autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# slstatus bar
 slstatus &
+
+# ibus for vietnamese
 ibus-daemon -d
-redshift
+
+# background image with feh
+feh --bg-scale ~/Pictures/backgrounds/b-011.jpg
+
+# redshift for night light
+redshift -O 4000
